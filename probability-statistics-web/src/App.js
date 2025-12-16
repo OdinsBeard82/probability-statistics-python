80a62484ae8544e3d0e7523c73bb411122a9ac9d
Daniel Charles <danielcharles_500@hotmail.com>
2025-12-16 09:30:12 +0000

    basic styling started

--- probability-statistics-web/src/App.js
+++ probability-statistics-web/src/App.js
@@ -3,10 +3,12 @@ import CoinFlip from './components/CoinFlip';
 import DiceRoll from './components/DiceRoll';
 import NumberRange from './components/NumberRange';
 import MontyHall from './components/MontyHall';
+import './App.css';
+
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>Week 1: Basic Probability Simulations</h1>
       <CoinFlip />
       <DiceRoll />
